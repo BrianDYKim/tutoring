2658b9ea43d1a4861e22cca54214801b7f662b2e
BrianDYKim <18k7102dy@naver.com>
2023-06-20 00:12:44 +0900

    add: gradle 설정 완료

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,23 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val mockkVersion = "1.12.0"
+val kotestVersion = "5.5.5"
+
 plugins {
-    id("org.springframework.boot") version "3.1.0"
-    id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.8.21"
-    kotlin("plugin.spring") version "1.8.21"
-    kotlin("plugin.jpa") version "1.8.21"
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    id("org.jetbrains.kotlin.plugin.allopen")
+    id("org.jetbrains.kotlin.plugin.noarg")
+
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    kotlin("plugin.jpa")
+
+    kotlin("kapt")
 }
 
 group = "me.marketdesigners"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -20,12 +28,34 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    // Kotlin Standard Library
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    runtimeOnly("com.mysql:mysql-connector-j")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Jackson
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.module:jackson-module-afterburner")
+
+    // SpringBoot
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // Spring actuator
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    // mockk
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion") // for kotest framework
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion") // for kotest core jvm assertions
+    testImplementation("io.kotest:kotest-property:$kotestVersion") // for kotest property test
+
+    // Database
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    // Annotation Processing Tool
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
 }
 
 tasks.withType<KotlinCompile> {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,20 @@
 rootProject.name = "assignment"
+
+pluginManagement {
+    val kotlinVersion = "1.8.21"
+    val springBootVersion = "3.1.0"
+    val dependencyManagementVersion = "1.1.0"
+
+    plugins {
+        id("org.springframework.boot") version springBootVersion
+        id("io.spring.dependency-management") version dependencyManagementVersion
+        id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
+        id("org.jetbrains.kotlin.plugin.noarg") version kotlinVersion
+
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.spring") version kotlinVersion
+        kotlin("plugin.jpa") version kotlinVersion
+
+        kotlin("kapt") version kotlinVersion
+    }
+}
