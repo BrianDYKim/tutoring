ec58475fb784ecbba896c3800e83b0e5b1c0f178
BrianDYKim <18k7102dy@naver.com>
2023-06-19 18:41:28 +0900

    Initial commit

---
+++ src/main/kotlin/me/marketdesigners/assignment/TutoringApplication.kt
@@ -0,0 +1,11 @@
+package me.marketdesigners.assignment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TutoringApplication
+
+fun main(args: Array<String>) {
+    runApplication<TutoringApplication>(*args)
+}
